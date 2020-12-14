f01094e956540c564512f5c5f1619b39df7a153c
Md : Zahangir Alam Joy <58134242+CsJoy0363@users.noreply.github.com>
2020-12-14 07:34:42 +0600

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include<isostream>
 using namespace std;
 int main() {
+    int age;
     cout << "Hellow World!";
+    cout << "How Old Are You?";
+    cinn >> "age";
+    cout << "Age : "age", %d;
     return 0;
 }
